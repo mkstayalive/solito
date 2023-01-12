4380b1bef8f8120962db856f43fa12c1908228a4
Death666Machine <Cody.Gagliardo12@gmail.com>
2023-01-11 18:56:42 -0800

    add with-expo-router-tailwind

---
+++ example-monorepos/with-expo-router-tailwind/apps/expo/babel.config.js
@@ -0,0 +1,12 @@
+module.exports = function (api) {
+  api.cache(true)
+  return {
+    presets: [['babel-preset-expo', { jsxRuntime: 'automatic' }]],
+    plugins: [
+      'react-native-reanimated/plugin',
+      // https://expo.github.io/router/docs/intro#configure-the-babel-plugin
+      require.resolve('expo-router/babel'),
+      'nativewind/babel',
+    ],
+  }
+}
